f3268e2ef0911f1f4e5c12dddcf6ce9825c2e485
gmeeraswamy <77478192+gmeeraswamy@users.noreply.github.com>
2021-02-06 10:56:58 +0530

    Add files via upload

---
+++ LetsUpgrade/assignment-1.py
@@ -0,0 +1,7 @@
+x = int(input("Enter x : "))
+y = int(input("Enter y : "))
+
+print("Addition : "+str(x+y))
+print("Multiplication : "+str(x*y))
+print("Subtraction : "+str(x-y))
+print("Division : "+str(x//y))
